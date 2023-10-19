15f3c296eb295f5807e72b80342878f495b596f7
PatoxProgramador <magalan942@gmail.com>
2023-10-19 10:19:38 +0200

    bullet movement

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
+    public float speed;
 
     void Start()
     {
@@ -17,7 +18,7 @@ public class Bullet : MonoBehaviour
     void Update()
     {
 
-        
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
         
     }
 
