2aee8b525d09b53e9da9bbc01227a0a7d8267a42
PatoxProgramador <magalan942@gmail.com>
2023-10-19 17:05:22 +0200

    a

--- Assets/Scripts/Testing/MovementCheck.cs
+++ Assets/Scripts/Testing/MovementCheck.cs
@@ -2,25 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class MovementCheck : MonoBehaviour
 {
-
-    Vector2 previousSpot;
    
     private float distance;
 
+    private Vector3 target;
+
+    public float speed = 5f;
+    public float wait;
 
     void Start()
     {
 
-        previousSpot = transform.position;
+        target = transform.position;
         
     }
     void Update()
     {
 
-        distance = Vector2.Distance(transform.position, previousSpot);
+        distance = Vector2.Distance(transform.position, target);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        StartCoroutine(Timer(wait));
+
+    }
+
+    IEnumerator Timer(float time)
+    {
+
+        if (distance < 1)
+        {
+
+            yield return new WaitForSeconds(time);
+
+            if (distance < 1)
+            {
+
+                print("NOT MOVING");
+
+            }
+            else
+            {
+
+                StartCoroutine(Timer(time));
+
+            }
+
+        }
 
     }
 
