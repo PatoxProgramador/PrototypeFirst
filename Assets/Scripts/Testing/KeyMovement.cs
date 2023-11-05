927f77f3c52762d21388509c43ed981828b024a2
PatoxProgramador <magalan942@gmail.com>
2023-11-05 20:16:56 +0100

    added loading mechanic

--- Assets/Scripts/Testing/KeyMovement.cs
+++ Assets/Scripts/Testing/KeyMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyMovement : MonoBehaviour
 {
@@ -10,10 +11,14 @@ public class KeyMovement : MonoBehaviour
 
     AIChase a;
 
+    public Image ed;
+
     void Start()
     {
 
-        
+        ed.fillAmount = 0f;
+
+        StartCoroutine(AB());
         
     }
 
@@ -48,6 +53,24 @@ public class KeyMovement : MonoBehaviour
         yield return new WaitForSeconds(3f);
 
         a.enabled = true;
+
+    }
+
+    IEnumerator AB()
+    {
+
+        ed.fillAmount += 0.25f;
+
+        yield return new WaitForSeconds(0.75f);
+
+        if (ed.fillAmount >0.9)
+        {
+
+            ed.fillAmount = 0.0f;
+
+        }
+
+        StartCoroutine(AB());
 
     }
 
